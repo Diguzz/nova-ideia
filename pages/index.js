24bcb836197809227dc08c92ca7f7f1eb92766f8
Rodrigo Henriques <rodrigobhenriques@gmail.com>
2021-01-21 17:37:08 -0300

    primeira pagina

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import {useState} from 'react';
 
+
 function Home(){
+
     return (
         <div>
-            <h3>Home</h3>            
+            <h1>Home</h1>
+            <br></br>
+            <button>Adicionar</button>
         </div>
     )
 }
